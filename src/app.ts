58b0d3b8280fd70b2f0c936cb1b624c6d1b09544
gustavoab94 <gustavoalvesabreu94@gmail.com>
2025-01-30 21:28:19 -0300

    test

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,17 @@
 import { PrismaClient } from '@prisma/client'
 import fastify from 'fastify'
+import { z } from 'zod'
 
 export const app = fastify()
 
 const prisma = new PrismaClient()
+
+app.post('/users', (request, reply) => {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
+
+  const { name, email, password } = registerBodySchema.parse(request.body)
+})
