59c142d0fe99c95b28f8f5968ff2deb2fab2ca18
gustavoab94 <gustavoalvesabreu94@gmail.com>
2025-02-19 17:29:01 -0300

    finish checkin repossitory

---
+++ src/use-cases/check-in.spec.ts
@@ -0,0 +1,105 @@
+import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins-repository'
+import { InMemoryGymsRepository } from '@/repositories/in-memory/in-memory-gyms-repository'
+import { CheckInUseCase } from '@/use-cases/check-in'
+import { Decimal } from '@prisma/client/runtime/library'
+import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest'
+
+let checkInsRepository: InMemoryCheckInsRepository
+let gymsRepository: InMemoryGymsRepository
+let sut: CheckInUseCase
+
+describe('Check-in Use Case', () => {
+  beforeEach(() => {
+    checkInsRepository = new InMemoryCheckInsRepository()
+    gymsRepository = new InMemoryGymsRepository()
+    sut = new CheckInUseCase(checkInsRepository, gymsRepository)
+
+    gymsRepository.items.push({
+      id: 'gym-01',
+      title: 'Gym test',
+      description: 'gym of vi tests',
+      latitude: new Decimal(0),
+      longitude: new Decimal(0),
+      phone: '319999999',
+    })
+
+    vi.useFakeTimers()
+  })
+
+  afterEach(() => {
+    vi.useRealTimers()
+  })
+
+  it('should be able to check in', async () => {
+    const { checkIn } = await sut.execute({
+      gymId: 'gym-01',
+      userId: 'user-01',
+      userLatitude: 0,
+      userLongitude: 0,
+    })
+
+    expect(checkIn.id).toEqual(expect.any(String))
+  })
+
+  it('should not be able to check in twice in the same day', async () => {
+    vi.setSystemTime(new Date(2022, 0, 20, 8, 0, 0))
+
+    await sut.execute({
+      gymId: 'gym-01',
+      userId: 'user-01',
+      userLatitude: 0,
+      userLongitude: 0,
+    })
+
+    await expect(() =>
+      sut.execute({
+        gymId: 'gym-01',
+        userId: 'user-01',
+        userLatitude: 0,
+        userLongitude: 0,
+      }),
+    ).rejects.toBeInstanceOf(Error)
+  })
+
+  it('should be able to check in twice but in different days', async () => {
+    vi.setSystemTime(new Date(2022, 0, 20, 8, 0, 0))
+
+    await sut.execute({
+      gymId: 'gym-01',
+      userId: 'user-01',
+      userLatitude: 0,
+      userLongitude: 0,
+    })
+
+    vi.setSystemTime(new Date(2022, 0, 21, 8, 0, 0))
+
+    const { checkIn } = await sut.execute({
+      gymId: 'gym-01',
+      userId: 'user-01',
+      userLatitude: 0,
+      userLongitude: 0,
+    })
+
+    expect(checkIn.id).toEqual(expect.any(String))
+  })
+
+  it('should not be able to check in on distant gym', async () => {
+    gymsRepository.items.push({
+      id: 'gym-02',
+      title: 'Gym test',
+      description: 'gym of vi tests',
+      latitude: new Decimal(10),
+      longitude: new Decimal(10),
+      phone: '319999999',
+    })
+
+    await expect(() =>
+      sut.execute({
+        gymId: 'gym-02',
+        userId: 'user-01',
+        userLatitude: 0,
+        userLongitude: 0,
+      }),
+    ).rejects.toBeInstanceOf(Error)
+  })
+})
---
+++ src/use-cases/get-user-profile.spec.ts
@@ -0,0 +1,37 @@
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
+import { hash } from 'bcryptjs'
+import { beforeEach, describe, expect, it } from 'vitest'
+import { ResourceNotFoundError } from './errors/resource-not-found-error'
+import { GetUserProfileUseCase } from './get-user-profile'
+
+let usersRepository = new InMemoryUsersRepository()
+let sut = new GetUserProfileUseCase(usersRepository)
+
+describe('Get user profile Use Case', async () => {
+  beforeEach(() => {
+    usersRepository = new InMemoryUsersRepository()
+    sut = new GetUserProfileUseCase(usersRepository)
+  })
+
+  it('should be able to get user profile', async () => {
+    const createdUser = await usersRepository.create({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password_hash: await hash('123456', 6),
+    })
+
+    const { user } = await sut.execute({
+      userId: createdUser.id,
+    })
+
+    expect(user.id).toEqual(expect.any(String))
+  })
+
+  it('should not be able to get user profile with invlid user Id', async () => {
+    await expect(() =>
+      sut.execute({
+        userId: 'not-existing-id',
+      }),
+    ).rejects.toBeInstanceOf(ResourceNotFoundError)
+  })
+})
